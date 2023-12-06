bdc923e56a952c132808621cfffc353697e553d3
luizd30 <luizd30@gmail.com>
2023-12-06 12:31:32 -0300

    feat: add index page

--- api/product.js
+++ api/product.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', async(req, res, next) => {
-    res.send("This is a product page!");
-})
+router.get("/", async (req, res, next) => {
+  res.render("index");
+});
 
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const product = require("./api/product");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 const port = 3000;
 
 app.use("/meu-curso-online", product);
